6f528ca90aaf1dab98ca7fed6e3f8dbb629a3bf7
MrGlam <elonglam@gmail.com>
2023-12-13 14:12:21 +0200

    add user controller functionality

---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secretKey } = require('../config/keys');
+const User = require('../models/User');
+
+async function signup(req, res) {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+async function login(req, res) {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const accessToken = jwt.sign({ username: user.username }, secretKey);
+    res.json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+async function logout(req, res) {
+  // Perform any logout-related actions (e.g., token blacklist)
+  res.json({ message: 'Logged out successfully' });
+}
+
+async function dashboard(req, res) {
+  res.json({ message: 'Welcome to the dashboard', user: req.user });
+}
+
+module.exports = {
+  signup,
+  login,
+  logout,
+  dashboard,
+};
